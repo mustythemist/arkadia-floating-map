384a458475839cba90d9b238160a45c71a819a99
tharindu <mustythemist@gmail.com>
2023-06-18 16:56:41 +0530

    purple frend added

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,31 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import NftData from "../components/common/nftData";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Login = () => {
-
+  const router = useRouter();
 
   const { data, address, isLoading, error } = NftData();
+
+  useEffect(() => {
+    console.log('address', address);
+    if (address) {
+      setTimeout(() => {
+        router.push('/');
+      }, 2000);
+    }
+  }, [address]);
 
   return (
     <div className="login-main">
       <div className="login-main__box">
         <img src="text-logo.webp" alt="" />
         <ConnectWallet />
-        <h2>{isLoading ? 'Loading...' : ''}</h2>
-        <h2>{address}</h2>
+        <div>
+          {/* <h2>{isLoading ? 'Loading...' : ''}</h2> */}
+          <h2>{address ? 'Waiting to load the map ...' : ''}</h2>
+        </div>
       </div>
     </div>
   );
